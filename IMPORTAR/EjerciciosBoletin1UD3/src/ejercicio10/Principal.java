40b04773d84c32ee9d4e7ec555553a93f89be72e
carlosromanabad <carlosromanabad@gmail.com>
2023-11-24 12:43:39 +0100

    yes

--- IMPORTAR/EjerciciosBoletin1UD3/src/ejercicio10/Principal.java
+++ IMPORTAR/EjerciciosBoletin1UD3/src/ejercicio10/Principal.java
@@ -149,10 +149,6 @@ public class Principal {
 			
 		
 		while(boton!=cero);
-			
-		
-		
-		
 	
 		}
 		
---
+++ IMPORTAR/EjerciciosBoletin1UD3/src/ejercicio11/Principal.java
@@ -0,0 +1,9 @@
+package ejercicio11;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
